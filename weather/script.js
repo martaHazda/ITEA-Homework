fc8c138cd261bcf212c7d4eb55512390ccc70498
Marta Hazda <martahazda@gmail.com>
2017-10-21 18:10:35 +0300

    weather task 1-3

---
+++ weather/script.js
@@ -0,0 +1,71 @@
+/*
+
+Потрібно обновити погоду дял кожного міса використовуючи ajax виклик.
+використовувати потрібно сервіс https://openweathermap.org/current
+
+Приклад посилань для отримання інформації про погоду
+
+http://api.openweathermap.org/data/2.5/weather?appid=1cbb9d3c2d3d13f34be51c51afe4b6cb&q=Lviv,ua
+http://api.openweathermap.org/data/2.5/weather?appid=1cbb9d3c2d3d13f34be51c51afe4b6cb&q=Kiev,ua
+http://api.openweathermap.org/data/2.5/weather?appid=1cbb9d3c2d3d13f34be51c51afe4b6cb&q=Odessa,ua
+
+інформацію про стна небу шукайте тут 
+"weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
+Іофрмацію про температуру 
+"main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
+
+1. Зробити запит на openweathermap.org за допоомогою апі і відобразити поточну погоду для кожного міста.
+2. Додати кнопку "Обновити", яка буде обновляти погоду в місті.
+3. Додати setInterval який буде обноялти погоду що 5хв.
+
+Додаткові завдання
+4. В елемент з класом "weather-pic" додати відповідний шаблон https://codepen.io/joshbader/pen/EjXgqr?q=weather&limit=all&type=type-pens
+в залежності від параметра data["weather"]["main"]
+
+5. Додати форму, щоб можна було додати нвое місто і відобразити його.
+
+*/
+
+function forecast(city) {
+
+    var apiCall = "http://api.openweathermap.org/data/2.5/weather?appid=1cbb9d3c2d3d13f34be51c51afe4b6cb&q=" + city + ",ua";
+    
+    var cityRequest = new XMLHttpRequest();
+
+    cityRequest.open("GET", apiCall , false);
+
+    cityRequest.send();
+
+    if(cityRequest.status != 200) {
+        console.log("error")
+    } else {
+        console.log(cityRequest.responseText);
+    }
+    var cityObject = JSON.parse(cityRequest.responseText);
+
+    var cityTemp = document.querySelector(".card-" + city + " .card-temp");
+    cityTemp.textContent = Math.round(cityObject.main.temp - 273);
+
+    var cityWeather = document.querySelector(".card-" + city + " .card-info");
+
+    cityWeather.textContent = cityObject.weather[0].main + " " + cityObject.weather[0].description;
+}
+
+
+function updateForecast () {
+    forecast("kiev");
+
+    forecast("lviv");
+
+    forecast("odessa");
+}
+
+updateForecast();
+
+var button = document.querySelector(".update");
+
+
+button.onclick = updateForecast;
+
+setInterval(updateForecast, 300000)
+
